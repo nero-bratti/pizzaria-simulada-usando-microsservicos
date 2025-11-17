5872bf4601f339e9385d87a2f9324193d8e4cca8
Nero Bratti <bratti.nero@gmail.com>
2025-11-17 23:29:40 +0000

    Código desenvovido em aula

---
+++ estoque-service/src/resources/data.sql
@@ -0,0 +1,21 @@
+-- Inserção dos ingredientes
+INSERT INTO ingredientes (id, descricao) VALUES (1, 'Disco de pizza');
+INSERT INTO ingredientes (id, descricao) VALUES (2, 'Porcao de tomate');
+INSERT INTO ingredientes (id, descricao) VALUES (3, 'Porcao de mussarela');
+INSERT INTO ingredientes (id, descricao) VALUES (4, 'Porcao de presunto');
+INSERT INTO ingredientes (id, descricao) VALUES (5, 'Porcao de calabresa');
+INSERT INTO ingredientes (id, descricao) VALUES (6, 'Molho de tomate (200ml)');
+INSERT INTO ingredientes (id, descricao) VALUES (7, 'Porcao de azeitona');
+INSERT INTO ingredientes (id, descricao) VALUES (8, 'Porcao de oregano');
+INSERT INTO ingredientes (id, descricao) VALUES (9, 'Porcao de cebola');
+
+-- Inserção dos itens de estoque
+INSERT INTO itensEstoque (id, quantidade, ingrediente_id) VALUES (1, 30, 1);
+INSERT INTO itensEstoque (id, quantidade, ingrediente_id) VALUES (2, 30, 2);
+INSERT INTO itensEstoque (id, quantidade, ingrediente_id) VALUES (3, 30, 3);
+INSERT INTO itensEstoque (id, quantidade, ingrediente_id) VALUES (4, 30, 4);
+INSERT INTO itensEstoque (id, quantidade, ingrediente_id) VALUES (5, 30, 5);
+INSERT INTO itensEstoque (id, quantidade, ingrediente_id) VALUES (6, 30, 6);
+INSERT INTO itensEstoque (id, quantidade, ingrediente_id) VALUES (7, 30, 7);
+INSERT INTO itensEstoque (id, quantidade, ingrediente_id) VALUES (8, 30, 8);
+INSERT INTO itensEstoque (id, quantidade, ingrediente_id) VALUES (9, 30, 9);
